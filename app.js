abf24d6bdd5832b466173d409092cbd495459486
Deepak Kumar Sinha <sinhadeepak1115study@gmail.com>
2022-02-19 12:00:27 +0530

    update app.js file.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -11,7 +11,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect('mongodb://localhost:27017/todolistDB');
 
+const itemsSchema = {
+  name:String
+}
+
+const Item  = mongoose.model('Item', itemsSchema)
 
 app.get("/", function(req, res) {
 
